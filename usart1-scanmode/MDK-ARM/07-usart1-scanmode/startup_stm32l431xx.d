4f501427a27a7e1793b37748cb413f00b6823309
DESKTOP-7MNB1LF\除却巫山不是云 <595131894@qq.com>
2021-01-15 23:23:22 +0800

    作业提交。

---
+++ usart1-scanmode/MDK-ARM/07-usart1-scanmode/startup_stm32l431xx.d
@@ -0,0 +1,1 @@
+07-usart1-scanmode\startup_stm32l431xx.o: startup_stm32l431xx.s
---
+++ usart1-scanmode/MDK-ARM/RTE/RTE_Components.h
@@ -0,0 +1,14 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: '07-usart1-scanmode' 
+ * Target:  '07-usart1-scanmode' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+#endif /* RTE_COMPONENTS_H */
